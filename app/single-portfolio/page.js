2ef5a515e523e7e565baadc0f066c1a95b46b2c3
Ariel Loza <arielloza71@gmail.com>
2025-03-22 01:09:54 -0600

    Update Full Site

--- app/single-portfolio/page.js
+++ app/single-portfolio/page.js
@@ -1,4 +1,5 @@
 import BreadCrumb from "@/components/common/Breadcrumb";
+import PortfolioDetails from "@/components/portfolio/single/PortfolioDetails";
 import Projects from "@/components/projects";
 function SinglePortfolio() {
 	return (
