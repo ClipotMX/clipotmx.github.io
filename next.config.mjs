3bc2f78cd46bdfb71a3d4c6d708967b005594650
Ariel Loza <arielloza71@gmail.com>
2025-08-17 03:38:39 -0600

    update Repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'clipotmx.github.io'; // Cambia esto por el nombre de tu repositorio
+
+const nextConfig = {
+  reactStrictMode: true,
+  
+  // Configuración para GitHub Pages
+  images: {
+    unoptimized: true
+  },
+  
+  // Solo aplicar basePath y assetPrefix en producción
+  ...(isProd && {
+    assetPrefix: `/${repoName}/`,
+    basePath: `/${repoName}`,
+  }),
+  
+  // Exportar como sitio estático
+  output: 'export',
+  
+  // Directorio de salida
+  distDir: 'dist',
+  
+  // Añadir trailing slash
+  trailingSlash: true,
+  
+  // Desactivar optimizaciones que no funcionan en GitHub Pages
+  swcMinify: true,
+};
 
 export default nextConfig;
