8c854b602e45216f74261a578e71fdfc0440d279
Ariel Loza <arielloza71@gmail.com>
2025-08-17 00:51:23 -0600

    update tag manager

--- app/layout.js
+++ app/layout.js
@@ -37,17 +37,21 @@ export const metadata = {
 export default function HomeOneLayout({ children }) {
   return (
     <html lang="en">
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+       <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PLSBG8GJ');
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
             `,
-        }}
-      />
+          }}
+        />
+      </head>
       <body className={`${syne.variable} ${inter.variable}`}>
         {/* Google Analytics */}
         <Script
